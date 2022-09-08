f96003796c4655472bce97046bd055775f0ad880
Osayimwense Iyamu <iyamuemmanuel1009@gmail.com>
2022-09-08 10:56:23 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,11 @@
 /**
  * main - print the project function
  *
- * Decription: this program prints "Programming is like building a multilingual puzzle"
+ * Decription: this program prints Programming is like building a multilingual puzzle
  * Return: 0
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0)
+	return (0);
 }
