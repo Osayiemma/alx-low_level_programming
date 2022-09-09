204cef1e09c9e2d968c85241b877a500e59ed501
Osayimwense Iyamu <iyamuemmanuel1009@gmail.com>
2022-09-09 10:51:34 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints the numbers from 0 to 9
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+for (i = '0'; i <= '9'; i++)
+{
+	putchar(n);
+}
+putchar('\n');
+return (0);
+}
