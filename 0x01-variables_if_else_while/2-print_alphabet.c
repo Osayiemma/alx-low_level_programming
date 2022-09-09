f851ca5a8cebf260226134a197d1c3fddda68f62
Osayimwense Iyamu <iyamuemmanuel1009@gmail.com>
2022-09-09 08:11:29 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print if alphabet is lower case
+ *
+ * Return: 0
+ */
+int main(void)
+{
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+	putchar(ch);
+}
+putchar('\n');
+return (0);
+}
